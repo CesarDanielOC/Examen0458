00f7d9255a83188f2d2783b2c9a7ad6451a5b732
CesarDanielOC <146128780+CesarDanielOC@users.noreply.github.com>
2024-09-27 09:55:23 -0600

    Add files via upload

--- HospitalOtikuroOtimato0458.py
+++ HospitalOtikuroOtimato0458.py
@@ -12,23 +12,23 @@ class Hospital0458:
         for x, y in Medicamento0458.items():
                 print(x, y)
                 Médico0458 = {
-"\nNombre del médico:": "José Alfredo ",
+"\nNombre del médico:": "José Alfredo \n",
 "ID_Médico:": 1,
-"Apellido": "Jimenez Alvarado",
-"Especialidad:": "Neurología",
-"Número de teléfono:": "656 147 8742",
-"Correo": "Josealfredo@gmail.com",
+"\nApellido": "Jimenez Alvarado\n",
+"Especialidad:": "Neurología\n",
+"Número de teléfono:": "656 147 8742\n",
+"Correo": "Josealfredo@gmail.com\n",
 "Fecha de contratación:": "7 de octubre 2019"
 }
         for x, y in Médico0458.items():
                 print(x, y)
                 Paciente0458 = {
-"\nNombre del paciente:": "Pepito ",
+"\nNombre del paciente:": "Pepito \n",
 "ID_Paciente:": 1,
-"Apellido": "Juanes",
-"Fecha de nacimiento:": "10 de noviembre 2015",
-"Número de teléfono:": "656 851 8411",
-"Dirección": "Calle del perro #8124",
+"\nApellido": "Juanes\n",
+"Fecha de nacimiento:": "10 de noviembre 2015\n",
+"Número de teléfono:": "656 851 8411\n",
+"Dirección": "Calle del perro #8124\n",
 "Fecha de registro:": "10 de noviembre 2016"
 }
         for x, y in Paciente0458.items():
@@ -37,10 +37,20 @@ class Hospital0458:
 "\nID_Receta:": 1,
 "ID_Paciente:": 1,
 "ID_Médico": "1",
-"Fecha de Receta:": "10 de noviembre 2016",
-"Instrucciones:": "Inyectar en glúteos cada 2 meses",
-"Validez": "Contactar a Josealvarado@gmail.com",
+"Fecha de Receta:": "10 de noviembre 2016\n",
+"Instrucciones:": "Inyectar en glúteos cada 2 meses\n",
+"Validez": "Contactar a Josealvarado@gmail.com\n",
 "ID_Medicamento:": 5
 }
         for x, y in Receta0458.items():
                 print(x, y)
+
+Objeto = Hospital0458()
+print("\nMedicamento")
+print(Hospital0458.Medicamento0458)
+print("\nMédico")
+print(Hospital0458.Médico0458)
+print("\nPaciente")
+print(Hospital0458.Paciente0458)
+print("\nMedicamento")
+print(Hospital0458.Receta0458)
